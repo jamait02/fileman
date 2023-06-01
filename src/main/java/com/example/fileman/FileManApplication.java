77a0f07e3fe2669b06d6dcc633353f1f2b1dcc0c
jaremari <jarema.marinez@neonex.de>
2023-06-01 17:32:55 +0200

    Initial Commit.

---
+++ src/main/java/com/example/fileman/FileManApplication.java
@@ -0,0 +1,13 @@
+package com.example.fileman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileManApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileManApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fileman/FileManApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fileman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileManApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
